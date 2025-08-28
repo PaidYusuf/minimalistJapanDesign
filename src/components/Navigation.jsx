25a77bc9fa3ac3dcaaf8699df2f2b50fc38a7bac
PaidYusuf <yusuf.aslan@std.antalya.edu.tr>
2025-08-28 23:42:31 +0300

    added english buttons for header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,18 +9,30 @@ const Navigation = ({ mainRef, aboutRef, portfolioRef, contactRef, showFullPortf
   return (
     <nav className="navigation">
       <div className="nav-item" onClick={() => scrollToSection(mainRef)}>
-        <span>主要</span>
+        <span className="nav-text">
+          <span className="nav-english">Main</span>
+          <span className="nav-japanese">主要</span>
+        </span>
       </div>
       {showFullPortfolio && (
         <>
           <div className="nav-item" onClick={() => scrollToSection(aboutRef)}>
-            <span>私について</span>
+            <span className="nav-text">
+              <span className="nav-english">About</span>
+              <span className="nav-japanese">私について</span>
+            </span>
           </div>
           <div className="nav-item" onClick={() => scrollToSection(portfolioRef)}>
-            <span>作品集</span>
+            <span className="nav-text">
+              <span className="nav-english">Portfolio</span>
+              <span className="nav-japanese">作品集</span>
+            </span>
           </div>
           <div className="nav-item" onClick={() => scrollToSection(contactRef)}>
-            <span>コンタクト</span>
+            <span className="nav-text">
+              <span className="nav-english">Contact</span>
+              <span className="nav-japanese">コンタクト</span>
+            </span>
           </div>
         </>
       )}
